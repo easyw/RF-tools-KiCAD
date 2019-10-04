a201abaf94405e5cf8198bb348d16b6bc6dcc089
easyw <easyw@katamail.com>
2019-10-04 09:39:16 +0200

    an other path improvements

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# pcbnew loads this folder as a package using import
+# thus __init__.py (this file) is executed
+# We import the plugin class here and register it to pcbnew
+
+from . import rf_tools_wizards
+
+
+
+
+
