1e869a1f0de7234a0b085014e1a50fe563482745
easyw <easyw@katamail.com>
2019-10-04 09:31:38 +0200

    moved to an easier install repo structure

---
+++ rf_tools_wizards/__init__.py
@@ -0,0 +1,11 @@
+# pcbnew loads this folder as a package using import
+# thus __init__.py (this file) is executed
+# We import the plugin class here and register it to pcbnew
+
+from . import uwArcPrimitive_wizard
+from . import uwMitered_wizard
+from . import uwTamper_wizard
+
+
+
+
